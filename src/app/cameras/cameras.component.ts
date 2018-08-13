d3b67d52f96b5c02a027c9062af86003c0439600
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-08-13 20:42:56 +0100

    tidy cameras component

--- src/app/cameras/cameras.component.ts
+++ src/app/cameras/cameras.component.ts
@@ -1,36 +1,10 @@
-import {Component, OnChanges, SimpleChanges, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'cameras',
   templateUrl: './cameras.component.html',
   styleUrls: ['./cameras.component.sass']
 })
-export class CamerasComponent implements OnChanges {
-  private _cameras = [];
-
-  constructor() {
-  }
-
-  @Input()
-  set cameras(cameras: Array<object>) {
-    this._cameras = cameras;
-  }
-
-  get cameras(): Array<object> {
-    return this._cameras;
-  }
-
-
-  ngOnInit() {
-    // console.log($("cameras img").naturalHeight, "foooo")
-    // debugger;
-    // console.log("foo")
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    // debugger;
-
-    console.log(changes, this)
-    // changes.prop contains the old and the new value...
-  }
+export class CamerasComponent {
+  @Input() cameras: Array<object>;
 }
